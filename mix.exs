b5960036724ddfc42fdc3ccbe9578d59a6bce610
dwieb <dwayne.harmon@gmail.com>
2019-03-20 03:08:27 -0500

    Installed ja_serializer

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule DwayneFM.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ja_serializer, "~> x.x.x"},
       {:corsica, "~> 1.0"},
       {:bcrypt_elixir, "~> 1.0"},
       {:phoenix, "~> 1.4.1"},
